275b95aea7aed2497c0b5c1bfad7af50d605f8b6
ameer maizin <alamiryamer@gmail.com>
2025-09-01 10:53:30 +0300

    final solution

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,23 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
+  // Allow all origins since you only have Swagger
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: true, // Allow ALL origins
   });
 
   const swagger = new DocumentBuilder()
     .setTitle('nest js course-api')
     .setDescription('E-commerce API')
-    .addServer('http://localhost:3000')
+    .addServer(process.env.API_URL || 'http://localhost:3000')
     .setTermsOfService('http://localhost:3000/terms-service')
     .setLicense('MIT license', 'google.com')
     .setVersion('1.0')
     .addSecurity('bearer', { type: 'http', scheme: 'bearer' })
     .addBearerAuth()
     .build();
-  const documentation = SwaggerModule.createDocument(app, swagger);
 
-  // 'http://localhost:3000/swagger'
+  const documentation = SwaggerModule.createDocument(app, swagger);
   SwaggerModule.setup('swagger', app, documentation);
 
   await app.listen(process.env.PORT ?? 3000);
